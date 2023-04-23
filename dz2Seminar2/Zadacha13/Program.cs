a9e1983b545948e34e498fabf80058bdbe0f2f94
Alexs <suhoff31it@gmail.com>
2023-04-23 18:32:11 +0300

    dz2Seminar2

---
+++ dz2Seminar2/Zadacha13/Program.cs
@@ -0,0 +1,25 @@
+﻿int ThirdDigit(int number)
+{
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        int result = number % 10;
+    }
+    else
+    {
+        int result = -1; // [B] Переменной "result" присвоено значение но оно ниразу не используется[/B]
+    }
+
+    return result; // [B]имя "result" не существует в текущем контексте[/B]
+}
+
+Console.Write("Input three-digit number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (ThirdDigit(number1) == -1)
+    Console.WriteLine("третьей цифры нет");
+else
+    Console.WriteLine($"Third digit is {ThirdDigit(number1)}");
---
+++ dz2Seminar2/Zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите номер дня недели :");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+
+if (num > 5 && num <8)
+{
+    Console.WriteLine("Выходной");
+}
+else if(num >0 && num <= 5)
+{
+    Console.WriteLine("Рабочий день");
+}
+else if (num > 7)
+{
+    Console.WriteLine("Не явдяется днем недели");
+}
